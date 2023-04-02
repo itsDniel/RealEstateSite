f1c28563e415ca1fa44ae942a73eb847c2a9596d
Daniel Doan <huydoan2306@gmail.com>
2023-04-01 22:39:43 -0400

    test

--- RealEstateSite/RealEstateSite/Search.aspx.designer.cs
+++ RealEstateSite/RealEstateSite/Search.aspx.designer.cs
@@ -222,6 +222,33 @@ namespace RealEstateSite
         protected global::System.Web.UI.WebControls.Repeater rprDisplay;
 
         /// <summary>
+        /// ProfilePanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel ProfilePanel;
+
+        /// <summary>
+        /// Repeater1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater1;
+
+        /// <summary>
+        /// ProfilePopUp control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.ModalPopupExtender ProfilePopUp;
+
+        /// <summary>
         /// ScriptManager1 control.
         /// </summary>
         /// <remarks>
